d28a78a2572990bf7e51fbbea7617804a84b1e1a
Asli Atici <e272958@edu.sbl.ch>
2026-03-13 09:27:08 +0100

    1 Video

--- src/main.js
+++ src/main.js
@@ -1,20 +1,9 @@
 import kaplay from "kaplay";
-import sc00 from "./scenes/sc-00";
-import sc01 from "./scenes/sc-01";
 
-const k = kaplay({
+const k = kaplay ({
 	height: 480,
 	width: 640,
-	canvas: document.getElementById("game-canvas"),
-	background: "#82b4b4",
-	global: false,
-	debug: true,
-	debugKey: "r",
+	canvas: document.getElementById ("game-canvas"),
+	background
+
 });
-
-k.scene("init", sc00);
-k.scene("lvl-01", sc01);
-
-k.go("init");
-
-export default k;
